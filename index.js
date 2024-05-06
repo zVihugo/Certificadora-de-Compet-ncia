8b793f65372a99643f1dc0430737d392c43b0173
Leandro Henrique <leandrohenriquetti@gmail.com>
2024-05-06 14:24:27 -0300

    Adição do banco de dados a aplicação
    Criação da pasta model, onde se encontrará o arquivo que contém o script necessário para a criação do nosso banco de dados. Banco criado com nome de "database.sqlite"

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("qualquer coisa asdnsiadnis");
+app.get("/", async (req, res) => {
+    res.send("Worked!");
 })
 
 app.listen(port, () => {
