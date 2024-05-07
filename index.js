bd0f6698b469b10c555ad7ec3dc718a0b411b346
Leandro Henrique <leandrohenriquetti@gmail.com>
2024-05-07 14:54:31 -0300

    Comentários para facilitar a compreensão da rota install

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ app.get("/install", async (req, res) => {
         userId: user.id
     })
 
+    let toDo3 = await todoService.newToDo({
+        titulo: "Minha terceira tarefa",
+        descricao: "Projeto competência",
+        dataEntrega: null,
+        userId: user.id
+    })
+
     //Encontrar um usuário por chave primária
     let findUser = await userService.findByPK(user.id)
 
@@ -47,14 +54,7 @@ app.get("/install", async (req, res) => {
     //Listar novamente as tarefas de um usuário
     newList = await todoService.listByUser(findUser.id)
 
-    //Criação de uma nova tarefa
-    let toDo3 = await todoService.newToDo({
-        titulo: "Minha terceira tarefa",
-        descricao: "Projeto competência",
-        dataEntrega: null,
-        userId: user.id
-    })
-
+    //Encontrar uma tarefa por ID
     let findToDo = await todoService.findByID(toDo3.id);
 
     res.json({msg: "Worked!", list: list, newList: newList, findToDo: findToDo});
